9583abc0c6af2783c70f588e50557a91cc37b4aa
Mahbuba-Akhter-Ema <mahbubaakhter22@gmail.com>
2022-08-04 19:28:10 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,12 @@
+// Declayer Function
+function startFan() {
+    console.log('stand up');
+    console.log('walk towards to the fan switch');
+    console.log('Press the switch');
+}
+// call the function
+startFan();
+console.log('Dont take too much time');
+startFan();
+console.log('Do it faster');
+startFan();
---
+++ parameter.js
@@ -0,0 +1,20 @@
+function bringSinggara(money) {
+    console.log('singgara den');
+    console.log('tk paisen:', money);
+}
+
+var taka = 300;
+// bringSinggara(300);
+bringSinggara(taka);
+
+// call 2 parameter
+function add(num1, num2) {
+    console.log('give me some money', num1, num2);
+}
+add(200, 300);
+
+function sum(a, b, c, d, e) {
+    var sum = a + b + c + d + e;
+    console.log(sum);
+}
+sum(5, 10, 15, 20, 25);
